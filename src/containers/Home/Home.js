f077b4c1e1a14241038a02f54663bf6ff13e0da3
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2022-10-02 11:34:11 +0400

    Create the home container

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='Home'>
+            <p>
+                This is the Homepage
+            </p>
+    </div>
+    )
+}
+
+export default Home;
