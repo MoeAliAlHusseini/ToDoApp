8ff9e218f4c551740ea7034edc8de22287c9f2c4
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2022-10-02 14:47:15 +0400

    Import Search input to home and make some changes

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+import SearchInput from '../../components/SearchInput'
 
 import './Home.css'
 
 const Home = () => {
+
+    const [newTask, setNewTask] = useState('');
+    const [listOfTasks, setListOfTasks] = useState([]);
+
+    useEffect(() => {
+        console.log("the homepage being rendered")
+        console.log("new task: ", newTask)
+        console.log("listOfTasks: ", listOfTasks)
+    });
+
+    const handleOnSubmit = () => {
+        // this function will handle the on submit functionality
+        if(listOfTasks.includes(newTask)) {
+            let updatedListOfTasks = listOfTasks.filter(item => item !== newTask)
+            setListOfTasks(updatedListOfTasks);
+        }
+        else {
+            setListOfTasks([...listOfTasks, newTask]);
+        }
+    }
+
     return (
         <div className='Home'>
             <p>
                 This is the Homepage
             </p>
+            <SearchInput onChange={setNewTask} value={newTask} onClear={() => setNewTask('')} onSubmit={handleOnSubmit} isSubmitDisabled={!newTask}/>
     </div>
     )
 }
