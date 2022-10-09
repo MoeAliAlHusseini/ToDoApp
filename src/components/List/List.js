0b263401557d9253e27774f004c6caaced706373
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2022-10-09 08:56:16 +0400

    Add trash icon and delete callback to list component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
+import Images from '../../images';
+
 import './List.css';
 
-const List = ({list = ['hello', 'yes', 'oui']}) => {
+const List = ({list = ['hello', 'yes', 'oui'], onDelete}) => {
     return (
-        <div style={{marginTop: 50}}>
+        <div className='list__wrapper'>
            {list.map((item, index) => {
             return (
                 <div className='text__wrapper'>
                      <p className='text'>
                         {item}
                     </p>
+                    <img className='icon' onClick={() => onDelete(index)} alt='trash' src={Images.trashicon} />
                 </div>
             )
            })}
